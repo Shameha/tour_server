7f960d8b709f151b08b4272c0df982d607f622db
shamehaera9 <shamehaera9@gmail.com>
2024-05-13 22:00:27 +0600

    added cookie

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+origin:[
+'http://localhost:5173',
+
+],
+credentials:true
+
+}));
 app.use(express.json());
 
 
@@ -32,6 +40,28 @@ async function run() {
    const volCollection = client.db('volDB').collection('volunteer');
    const beCollection =  client.db('volDB').collection('beVolunteer');
   
+//auth related api
+app.post('/jwt',async(req,res)=>{
+const user = req.body;
+console.log("user for token",user);
+const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'2h'})
+res.cookie('token',token,{
+  httpOnly:true,
+  secure:true,
+  sameSite: 'none'
+})
+.send({success:true})
+
+})
+
+app.post('/logout',async(req,res)=>{
+  const user = req.body;
+  console.log('loging out',user);
+  res.clearCookie('token',{maxAge: 0}).send({success:true})
+})
+
+
+
    app.get('/volunteer',async(req,res)=>{
      const cursor = volCollection.find();
     const result = await cursor.toArray();
